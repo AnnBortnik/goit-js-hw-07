0890efc21707b6547134baecea84feaca64a938d
AnnBortnik <libra.merie.panda@gmail.com>
2023-10-13 13:43:04 +0200

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,10 +2,10 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const listEl = document.querySelector('.gallery');
-const markUp = galleryItems.map(({preview, original, desription}) => {
+const markUp = galleryItems.map(({preview, original, description}) => {
     return `<li class='gallery__item'>
     <a class='gallery__link' href='${original}'>
-    <img loading='lazy' class='gallery__image' src='${preview} title='${desription}'/>
+    <img loading='lazy' class='gallery__image' src='${preview}' alt='${description}'/>
     </a>
     </li>`;
 }).join('');
@@ -13,10 +13,11 @@ const markUp = galleryItems.map(({preview, original, desription}) => {
 listEl.insertAdjacentHTML('beforeend', markUp);
 
 let lightbox = new SimpleLightbox('.gallery a', {
-    captions: true,
-    captionDelay: 250,
+    caption: true,
     fadeSpeed: 250,
-    captionSelector: "img",
-    captionsData: 'alt',
     captionPosition: 'bottom',
+    captionsData: 'alt', 
+    captionDelay: 250,
+    close: false,
+    enableKeyboard: true,
 });
