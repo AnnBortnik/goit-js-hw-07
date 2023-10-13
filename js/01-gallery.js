046c5f8e481d1d03ed2b031b8f41570ddb87061e
AnnBortnik <libra.merie.panda@gmail.com>
2023-10-13 11:15:59 +0200

    01 full + 02 a little

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const newImages = [];
+
+const newGallery = (object) => {
+    object.map(({preview, original, description}) => {
+        newImages.push(`<li style='margin: 25px;' class='gallery__item'>
+        <a class='gallery__link' href='${original}'>
+        <img loading='lazy' class='gallery__image' data-source='${original}' src='${preview}' alt='${description}'/>
+        </a>
+        </li>`);
+    });
+    gallery.insertAdjacentHTML('beforeend', newImages.join(''));
+};
+newGallery(galleryItems);
+
+gallery.addEventListener('click', modalGallery);
+function modalGallery(event) {
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG') return;
+    const instance = basicLightbox.create(`<img src='${event.target.dataset.source}'>`, {
+        onShow: () => {window.addEventListener('keydown', escClick)},
+        onClose: () => {window.removeEventListener('keydown', escClick)}
+    });
+
+    function escClick (event) {
+        if(event.code === 'Escape') {
+            instance.close();
+        }
+    }
+    instance.show();
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+const markUp = galleryItems.map(({preview, original, desription}) => {
+    return `<li class='gallery__item'>
+    <a class='gallery__link' href='${original}'>
+    <img loading='lazy' class='gallery__image' src='${preview} title='${desription}'/>
+    </a>
+    </li>`;
+}).join('');
+
+listEl.insertAdjacentHTML('beforeend', markUp);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    fadeSpeed: 250,
+    captionSelector: "img",
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+});
